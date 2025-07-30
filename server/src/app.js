6c1dbd8520e316e427475258ed59a89a8f0b6757
Siam Sheikh <siammsheikh09@gmail.com>
2025-07-31 01:01:06 +0600

    Server setup with mongoose

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
 import userRoutes from './routes/user.routes.js';
+import { notFound, errorHandler } from './middlewares/error.middleware.js';
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(helmet());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // API Routes
 app.use('/api/users', userRoutes);
@@ -15,5 +23,9 @@ app.use('/api/users', userRoutes);
 app.get('/', (req, res) => {
   res.send('Tasko server is running!');
 });
+
+// Error Handling Middleware
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,27 +1,16 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env' });
 
-const uri = process.env.MONGO_URI;
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
 const connectDB = async () => {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } catch (err) {
-    console.error("MongoDB connection failed:", err);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
 };
 
-export const dbClient = client;
 export default connectDB;
